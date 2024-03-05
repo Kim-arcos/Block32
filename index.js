3d5487f1e0dcc76a82b75194349e0cb20e4c75ac
Kim-arcos <kimarcos16@gmail.com>
2024-03-04 17:30:21 -0700

    resubmitting with changes

--- index.js
+++ index.js
@@ -1,44 +1,102 @@
-const express = require('express');
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_icecream_shop_db"
+);
+const express = require("express");
 const app = express();
-const port = 3000;
 
+// parse the body into JS Objects
+app.use(express.json());
 
+// Log the requests as they come in
+app.use(require("morgan")("dev"));
 
-CREATE TABLE flavors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    is_favorite BOOLEAN DEFAULT false,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-  );
-
-  INSERT INTO flavors (name, is_favorite) VALUES ('Chocolate', true);
-  INSERT INTO flavors (name, is_favorite) VALUES ('Vanilla', false);
-  INSERT INTO flavors (name, is_favorite) VALUES ('Strawberry', true);
-
-app.get('/api/flavors', (req, res) => {
-    res.send('Get all flavors');
+// Create flavor - C
+app.post("/api/flavor", async (req, res, next) => {
+  try {
+    const SQL = `
+      INSERT INTO flavor(name)
+      VALUES($1)
+      RETURNING *
+    `;
+    const response = await client.query(SQL, [req.body.name]);
+    res.send(response.rows[0]);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
-app.get('/api/flavors/:id', (req, res) => {
-    const flavorId = req.params.id;
-    res.send(`Get flavor with ID ${flavorId}`);
+// Read flavor - R
+app.get("/api/flavor", async (req, res, next) => {
+  try {
+    const SQL = `
+      SELECT * from flavor ORDER BY created_at DESC;
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
-app.post('/api/flavors', (req, res) => {
-    res.send('Create a new flavor');
+// Update flavor - U
+app.put("/api/flavor/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+      UPDATE flavor
+      SET name=$1, updated_at= now()
+      WHERE id=$3 RETURNING *
+    `;
+    const response = await client.query(SQL, [
+      req.body.name,
+      req.params.id,
+    ]);
+    res.send(response.rows[0]);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
-app.delete('/api/flavors/:id', (req, res) => {
-    const flavorId = req.params.id;
-    res.send(`Delete flavor with ID ${flavorId}`);
+// Delete flavor - D
+app.delete("/api/flavor/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+      DELETE from flavor
+      WHERE id = $1
+    `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
-app.put('/api/flavors/:id', (req, res) => {
-    const flavorId = req.params.id;
-    res.send(`Update flavor with ID ${flavorId}`);
-});
+// create and run the express app
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+const init = async () => {
+  await client.connect();
+  let SQL = `
+    DROP TABLE IF EXISTS flavor;
+    CREATE TABLE flavor(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      is_favorite BOOLEAN NOT NULL,
+      created_at TIMESTAMP DEFAULT now(),
+      updated_at TIMESTAMP DEFAULT now()
+    );
+  `;
+  await client.query(SQL);
+  console.log("tables created");
+  SQL = `
+  INSERT INTO flavor(name, is_favorite) VALUES
+    ('learn express', 'false'),
+    ('write SQL queries', 'false'),
+    ('create routes', 'false');
+`;
+  await client.query(SQL);
+  console.log("data seeded");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
